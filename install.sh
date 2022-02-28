348061271df82d1d78612f6f88bf80ebdb9290cc
Pathetic Geek <geekpathetic@gmail.com>
2022-02-28 11:52:35 +0530

    ✨ Add tmux and nicer install script

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env zsh
 
-STOW_FOLDERS=alacritty,kitty,neofetch,others,themes,vim,zsh
+STOW_FOLDERS=${STOW_FOLDERS:=alacritty,kitty,neofetch,others,vim,zsh,tmux}
 
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do
+    echo "Setting up ${folder}"
     stow -D $folder
     stow $folder
 done
+
+echo "✨ All done"
