db3562b8f4a6b932897d4f45626c71679afe1a46
Pathetic Geek <geekpathetic@gmail.com>
2023-09-18 04:47:40 +0530

    the dotfiles command

--- README.md
+++ README.md
@@ -3,24 +3,27 @@
 ### Installation
 
 1. Whichever platform you are on make sure you have `stow` and `git`
+
+   For ubuntu `sudo apt install git stow`
+
 2. Clone this repo in home dir
-    ```
-    git clone https://github.com/patheticGeek/.dotfiles
-    ```
+   ```
+   git clone https://github.com/patheticGeek/.dotfiles
+   ```
 3. Run the install script
-    ```
-    cd ~/.dotfiles
-    ./install.sh
-    ```
-    To install with personal (its a private git repo)
-    ```
-    cd ~/.dotfiles
-    PERSONAL=true ./install.sh
-    ```
+   ```
+   cd .dotfiles && ./dotfiles.sh setup
+   ```
+   To install with personal (its a private git repo for me)
+   ```
+   cd .dotfiles && ./dotfiles.sh setup --personal
+   ```
+
+Now `./dotfiles.sh` script will also be available as `dotfiles` as it is aliased in shell.
 
 ### Some explanation
 
-I'm using _stow_ to manage the symlinks.
+I'm using _stow_ to manage the symlinks and _git_ to version and backup.
 
 This is inspired by https://www.youtube.com/watch?v=tkUllCAGs3c
 
@@ -32,19 +35,47 @@ and then run `stow zsh` it will create symlinks at `~/.zshrc` and `~/.oh-my-zsh/
 
 The `personal` folder is a submodule, it is a private repo where i have my _personal_ keys and stuff.
 
-### How to add more stuff
+### How to add/delete more stuff
 
-1. Create a folder in `~/.dotfiles`
-    ```
-    mkdir ~/.dotfiles/vim
-    ```
-2. Move the files related to that in the folder
-    ```
-    mv ~/.vimrc ~/.dotfiles/vim/
-    ```
-3. Then create the symlink with stow
-    ```
-    stow vim
-    ```
+Can do so via `dotfiles [add|delete] FOLDER FILE` command. e.g.
 
-✨ And you're done
+```
+# Will add file at correct path to others folder
+dotfiles add others ~/.config/mimeapps.list
+```
+
+```
+# Will remove file from others folder and put it at original path
+dotfiles delete others ~/.config/mimeapps.list
+```
+
+### `dotfiles` command help
+
+```
+COMMAND
+    dotfiles [setup|remove|update] [--folders VALUE] [-p|--personal] [-d|--dry]
+    dotfiles add FOLDER FILE
+    dotfiles [-h|--help|help]
+
+DESCRIPTION
+    dotfiles command is supposed to be used to backup and sync your dotfiles using git & stow.
+
+OPTIONS
+    setup, remove, update
+        Pretty self explanatory.
+
+    add
+        Add an existing FILE. Will move FILE to the FOLDER in dotfiles with correct path and link it.
+
+    -h, --help, help
+        Will print this.
+
+    --folders VALUE
+        Takes a comma seperated list of folders that are to be used.
+
+    -p, --personal
+        If present setup the personal folder also.
+
+    -d, --dry
+        If present, this only does a dry run and outputs what will happen.
+```
--- dotfiles.sh
+++ dotfiles.sh
@@ -1,18 +1,181 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-STOW_FOLDERS=${STOW_FOLDERS:=common,bash,alacritty,fish,kitty,neofetch,others,vim,zsh,tmux,gestures}
+stowFolders=(common bash alacritty fish kitty neofetch others vim zsh tmux gestures)
+stowFlags=""
+personal=0
 
-if [ $PERSONAL ]
-then
-    STOW_FOLDERS=$STOW_FOLDERS,personal
-fi
+addFolder=
+addFile=
 
-STOW_PACKAGES=$(echo $STOW_FOLDERS | sed "s/,/ /g")
+_help() {
+    echo """
+COMMAND
+    dotfiles [setup|remove|update] [--folders VALUE] [-p|--personal] [-d|--dry]
+    dotfiles [add|delete] FOLDER FILE
+    dotfiles [-h|--help|help]
 
-echo Running for $STOW_PACKAGES
+DESCRIPTION
+    dotfiles command is supposed to be used to backup and sync your dotfiles using git & stow.
 
-STOW_FLAGS=${STOW_FLAGS:="-nv"}
+OPTIONS
+    setup, remove, update
+        Pretty self explanatory.
 
-stow $(echo $STOW_FLAGS) --adopt -S $(echo $STOW_PACKAGES)
+    add
+        Add an existing FILE. Will move FILE to the FOLDER in dotfiles with correct path and link it.
 
-echo "✨ All done"
+    delete
+        Stop tracking FILE. Will move the FILE to its location and restow the FOLDER removing the linking.
+
+    -h, --help, help
+        Will print this.
+
+    --folders VALUE
+        Takes a comma seperated list of folders that are to be used.
+
+    -p, --personal 
+        If present setup the personal folder also.
+
+    -d, --dry
+        If present, this only does a dry run and outputs what will happen.
+"""
+}
+
+_setup() {
+    echo Setting up - $stowFolders
+
+    stow $stowFlags --adopt -S $stowFolders
+
+    echo "✨ All done"
+}
+
+_remove() {
+    echo Removing - $stowFolders
+
+    stow $stowFlags -D $stowFolders
+
+    echo "✨ All done"
+}
+
+_update() {
+    echo Updating - $stowFolders
+
+    stow $stowFlags --adopt -R $stowFolders
+
+    echo "✨ All done"
+}
+
+createIfNotExists() {
+    test -d ${1} || mkdir -p ${1}
+}
+
+_add() {
+    if [[ -z $addFile ]]; then echo Require the FILE argument && exit 1; fi;
+    if [[ -z $addFolder ]]; then echo Require the FOLDER argument && exit 1; fi;
+    if ! [[ -f $addFile ]]; then echo File does not exist && exit 1; fi;
+
+    local relativePathToHome=$(realpath --relative-to="$HOME" $addFile)
+    local moveTo=~/.dotfiles/$addFolder/$relativePathToHome
+
+    # Move the file in the dotfiles
+    echo Moving $addFile to $moveTo
+    createIfNotExists $(dirname $moveTo)
+    mv $addFile $moveTo
+
+    # Setup that folder to link the file
+    stowFolders=($(echo $addFolder))
+    _setup
+}
+
+_delete() {
+    if [[ -z $addFile ]]; then echo Require the FILE argument && exit 1; fi;
+    if [[ -z $addFolder ]]; then echo Require the FOLDER argument && exit 1; fi;
+
+    local filePath=$HOME/.dotfiles/$addFolder/$addFile
+    if ! [[ -f $filePath ]]; then echo File does not exist && exit 1; fi;
+
+    local relativePathToHome=$(realpath --relative-to="$HOME/.dotfiles/$addFolder" $filePath)
+    local moveTo=$HOME/$relativePathToHome
+
+    # Move the file from dotfiles to correct path
+    echo Moving $filePath to $moveTo
+    if [[ -f $filePath ]]; then; rm $moveTo; fi;
+    mv $filePath $moveTo
+
+    # Setup that folder once again
+    stowFolders=($(echo $addFolder))
+    _setup
+}
+
+main() {
+    local position=0
+    local fnToRun="_help"
+
+    while [[ "${#}" -gt 0 ]]; do
+        case "${1}" in
+            -h|--help|help)
+                _help
+                exit 0
+            ;;
+            setup)
+                fnToRun="_setup"
+                shift
+            ;;
+            remove)
+                fnToRun="_remove"
+                shift
+            ;;
+            update)
+                fnToRun="_update"
+                shift
+            ;;
+            add)
+                fnToRun="_add"
+                shift
+            ;;
+            delete)
+                fnToRun="_delete"
+                shift
+            ;;
+            -f|--folders)
+                folders=${2}
+                stowFolders=($(echo $folders | sed "s/,/ /g"))
+                shift 2
+            ;;
+            -p|--personal)
+                personal=1
+                shift
+            ;;
+            -d|--dry)
+                stowFlags="-nv"
+                shift
+            ;;
+            *)
+                case "${position}" in
+                    0)
+                        addFolder="${1}"
+                        position=1
+                        shift
+                    ;;
+                    1)
+                        addFile="${1}"
+                        position=2
+                        shift
+                    ;;
+                esac
+            ;;
+        esac
+    done
+
+    [[ $personal -gt 0 ]] && stowFolders+=(personal)
+
+    stowFolders="${stowFolders[@]}" # Fix iteration shit in bash
+
+    # echo Running $fnToRun with folders=$stowFolders flags=$stowFlags addFolder=$addFolder addFile=$addFile
+
+    $fnToRun # Run whatever needs to run
+
+    return 0
+}
+
+main "${@:-}"
